db98e4c7f30113b786acdaed6b80f997918b5840
Anuvedya <anuvedya9@gmail.com>
2023-09-26 19:32:45 +0530

    first commit

---
+++ PalindromeLeetCode.java
@@ -0,0 +1,28 @@
+class Solution{
+
+    public boolean isPalindrome(int x){
+boolean op=false;
+int rem,sum=0,temp;
+temp=x;
+
+while(x>0){
+rem = x%10;
+sum = (sum * 10)+rem;
+x=x/10;
+}
+
+if(temp==sum) {op=true;}
+
+else {op=false;}
+        
+return op;  
+}
+}
+
+
+public class PalindromeLeetCode {
+    public static void main(String[] args) {
+        Solution s= new Solution();
+        System.out.println(s.isPalindrome(-121));
+    }
+}
