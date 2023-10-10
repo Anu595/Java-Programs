da6a7d763f03f3eb240b0192d8136f8d7e05b651
Anuvedya <anuvedya9@gmail.com>
2023-10-10 05:32:45 +0530

    cumulative sum of array commit

---
+++ cumulativeSum.java
@@ -0,0 +1,27 @@
+
+class Solution {
+	int[] getCumulativeSum (int[] arr) {
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+            arr[i] = sum;
+            System.out.println(arr[i]);
+        }
+
+        return arr;
+		
+	}
+}
+
+public class cumulativeSum {
+    public static void main(String[] args) {
+        
+    Solution s = new Solution();
+    int inp = 3;
+    int[] arr1={1,2,3,4};
+    int[] arr2 = {1,1,1,1,1};
+    int[] arr3 = {1,3,5,7,9};
+    s.getCumulativeSum(arr1);
+
+    }
+}
